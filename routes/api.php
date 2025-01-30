eaad6dacd20192467845c2f4edc2142ccfc256a6
Israël BOCO <66666854+israelboco@users.noreply.github.com>
2025-01-30 09:02:28 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ Route::post('auth/login', [AuthController::class, 'login'])->name('login');
 Route::post('auth/register', [AuthController::class, 'register'])->name('register');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::post('/user', [AuthController::class, 'getUser'])->name('user');
+    Route::get('auth/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/user', [AuthController::class, 'getUser'])->name('user');
 
 });
